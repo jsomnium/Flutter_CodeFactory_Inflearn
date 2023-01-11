a3305d87d659bde632dec4b0a07f29aceee9db16
Jisu <jsomnium@naver.com>
2023-01-11 22:58:22 +0900

    first commit

---
+++ satefulwidget_study/android/app/src/main/kotlin/com/example/satefulwidget_study/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.satefulwidget_study
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- web_view/lib/screen/HomeScreen.dart
+++ web_view/lib/screen/HomeScreen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('HomeScreen'),
+        child: Container(child: Text('HomeScreen')),
       ),
     );
   }
