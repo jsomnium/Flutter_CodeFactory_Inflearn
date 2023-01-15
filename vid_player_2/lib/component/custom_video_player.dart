7b7c672f1c9e05e646089f3b962df1c02fdfd536
Jisu <jsomnium@naver.com>
2023-01-15 22:15:33 +0900

    Feat: 비디오 플레이어 기능 추가
    1) 비디오 화면 비율 맞추기
    2) 3초 앞, 뒤로 갈 수 있는 버튼 만들기
    3) 정지, 재생 버튼 만들기

--- vid_player_2/lib/component/custom_video_player.dart
+++ vid_player_2/lib/component/custom_video_player.dart
@@ -40,8 +40,99 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
     if (videoController == null) {
       return CircularProgressIndicator(); // 로딩바
     }
-    return VideoPlayer(
-      videoController!,
+    return AspectRatio(
+      aspectRatio: videoController!.value.aspectRatio,
+      child: Stack(
+        children: [
+          VideoPlayer(
+            videoController!,
+          ),
+          _Controls(
+            onPlayPressed: onPlayPressed,
+            onReversePressed: onReversePressed,
+            onForwardPressed: onForwardPressed,
+            isPlaying: videoController!.value.isPlaying,
+          ),
+          Positioned(
+            right: 0, // 오른쪽 픽셀에서 0픽셀만큼 이동
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.photo_camera_back,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onPlayPressed() {}
+
+  void onReversePressed() {}
+
+  void onForwardPressed() {
+    // 이미 실행중이면 중지
+    // 실행중이 아니면 실행
+    setState(() {
+      if (videoController!.value.isPlaying) {
+        videoController!.pause();
+      } else {
+        videoController!.play();
+      }
+    });
+  }
+}
+
+class _Controls extends StatelessWidget {
+  final VoidCallback onPlayPressed;
+  final VoidCallback onReversePressed;
+  final VoidCallback onForwardPressed;
+  final bool isPlaying;
+
+  const _Controls(
+      {required this.onPlayPressed,
+      required this.onReversePressed,
+      required this.onForwardPressed,
+      required this.isPlaying,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black.withOpacity(0.5),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          renderIconButton(
+            onPressed: onReversePressed,
+            icondata: Icons.rotate_left,
+          ),
+          renderIconButton(
+            onPressed: onPlayPressed,
+            icondata: isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
+          renderIconButton(
+            onPressed: onForwardPressed,
+            icondata: Icons.rotate_right,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget renderIconButton({
+    required VoidCallback onPressed,
+    required IconData icondata,
+  }) {
+    return IconButton(
+      onPressed: onPressed,
+      icon: Icon(icondata),
+      iconSize: 30,
+      color: Colors.white,
     );
   }
 }
