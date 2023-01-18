ea7c8f2bddc5e4f4636b1242cb912a5c23cefe55
Jisu <jsomnium@naver.com>
2023-01-18 11:22:11 +0900

    Feat: Package 설치 완료
    googleMaps, geolocator 설치 완료

--- chool_check/ios/Runner/AppDelegate.swift
+++ chool_check/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCfsSxQ27k5r6HawQIqOsn9DfS2J7FZDrk")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
