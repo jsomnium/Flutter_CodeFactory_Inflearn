25203d255ffa69a4a44d2b1fd71260157b9eebda
Jisu <jsomnium@naver.com>
2023-01-18 10:52:02 +0900

    Update custom_video_player.dart

--- vid_player_2/lib/component/custom_video_player.dart
+++ vid_player_2/lib/component/custom_video_player.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/container.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:matcher/matcher.dart';
 import 'package:video_player/video_player.dart';
