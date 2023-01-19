4e442b085ab630a0cb84b32363a6c0cd9044fb90
Jisu <jsomnium@naver.com>
2023-01-20 00:32:17 +0900

    TEST

--- live_stream/lib/main.dart
+++ live_stream/lib/main.dart
@@ -4,6 +4,10 @@ import 'screen/HomeScreen.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
       home: HomeScreen(),
     ),
   );
