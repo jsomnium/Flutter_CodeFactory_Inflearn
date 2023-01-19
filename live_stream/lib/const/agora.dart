af482807e5625310a6896aff55653201674c2ef3
Jisu <jsomnium@naver.com>
2023-01-19 11:38:09 +0900

    Fetch: 기본 세팅 완료
    asset 폴더 세팅, package 파일 세팅, 스크린 화면 세팅 완료

---
+++ live_stream/lib/const/agora.dart
@@ -0,0 +1,4 @@
+const App_ID = '9e0239568de148a0919113dc3fc83cd2';
+const App_certificate = '3339b726f8804ea6af4117f5ee1fdf57';
+const Temp_Tocken =
+    '007eJxTYIhSWWmzX9fs+uObuQHXv52eHS7SbuVgW7Fl23LJ/fGvlf4rMFimGhgZW5qaWaSkGppYJBpYGloaGhqnJBunJVsYJ6cY8S87kdwQyMggsT2HgREKQXxuhpLU4hLnjMS8vNQcBgYAWlkiag==';
