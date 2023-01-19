5a57f1fa74da0643bd24bf40252ef380b97addad
Jisu <jsomnium@naver.com>
2023-01-20 02:09:46 +0900

    Fetch: 캘린더 구현

---
+++ calendar_study/lib/component/calendar.dart
@@ -0,0 +1,72 @@
+import 'package:calendar_study/const/color.dart';
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendar extends StatefulWidget {
+  const Calendar({super.key});
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  DateTime? selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    final defaultBoxDeco = BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+      color: Colors.grey[200],
+    );
+    final defaultTextStyle = TextStyle(
+      color: Colors.grey[600],
+      fontWeight: FontWeight.w700,
+    );
+
+    return TableCalendar(
+      firstDay: DateTime.utc(2000, 1, 1),
+      lastDay: DateTime.utc(2050, 12, 31),
+      focusedDay: DateTime.now(),
+      headerStyle: const HeaderStyle(
+        formatButtonVisible: false,
+        titleCentered: true,
+        titleTextStyle: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 16,
+        ),
+      ),
+      calendarStyle: CalendarStyle(
+          isTodayHighlighted: false,
+          defaultDecoration: defaultBoxDeco,
+          weekendDecoration: defaultBoxDeco,
+          selectedDecoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+            border: Border.all(
+              width: 1,
+              color: PRIMARY_COLOR,
+            ),
+          ),
+          defaultTextStyle: defaultTextStyle,
+          weekendTextStyle: defaultTextStyle,
+          selectedTextStyle: defaultTextStyle.copyWith(
+            color: PRIMARY_COLOR,
+          )),
+      onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+        setState(
+          () {
+            this.selectedDay = selectedDay;
+          },
+        );
+      },
+      selectedDayPredicate: (DateTime date) {
+        if (selectedDay == null) {
+          return false;
+        }
+        return date.year == selectedDay!.year &&
+            date.month == selectedDay!.month &&
+            date.day == selectedDay!.day;
+      },
+    );
+  }
+}
--- calendar_study/lib/main.dart
+++ calendar_study/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-
 import 'screen/homescreen.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
       home: HomeScreen(),
     ),
   );
