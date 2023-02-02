606205bb44d39d4099b4ef76fcc83cd156372963
Jisu <jsomnium@naver.com>
2023-02-02 11:50:58 +0900

    calendar error

--- calendar_study/lib/screen/homescreen.dart
+++ calendar_study/lib/screen/homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:calendar_study/component/calendar.dart';
+import 'package:calendar_study/component/schedule_card.dart';
 import 'package:calendar_study/component/today_banner.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -32,7 +33,17 @@ class _HomeScreenState extends State<HomeScreen> {
             TodayBanner(
               selectedDay: DateTime.now(),
               scheduleCount: 3,
-            )
+            ),
+            SizedBox(
+              height: 6,
+            ),
+            ScheduleCard(
+                      startTime: scheduleWithColor.schedule.startTime,
+                      endTime: scheduleWithColor.schedule.endTime,
+                      content: scheduleWithColor.schedule.content,
+                      color: Color(
+                        int.parse('FF${scheduleWithColor.categoryColor.hexCode}',
+                            radix: 16),
           ],
         ),
       ),
